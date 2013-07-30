59e5b05775fd696d6af04b300dea8661008a1e41
Matthijs Mekking <github@pletterpet.nl>
2013-07-30 14:59:44 +0200

    read wireformat data (uint32 only for now)

---
+++ signer/src/dns/wf.h
@@ -0,0 +1,48 @@
+/*
+ * $Id: wf.h 6501 2012-08-06 10:52:03Z matthijs $
+ *
+ * Copyright (c) 2013 NLNet Labs. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * Wire format access.
+ *
+ */
+
+#ifndef DNS_WF_H
+#define DNS_WF_H
+
+#include <stdint.h>
+
+/**
+ * Read 32 bits of wire format data.
+ * @param data: wire format data.
+ * @return:     (uint32_t) 32 bits of data.
+ *
+ */
+uint32_t wf_read_uint32(const void* data);
+
+#endif /* DNS_WF_H */
+
