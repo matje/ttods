a066e14033f687a70f2c5ddbf1b5b3bf5ddc70a5
Matthijs Mekking <github@pletterpet.nl>
2013-07-31 11:04:41 +0200

    add domain struct stuff

---
+++ signer/src/signer/domain.c
@@ -0,0 +1,172 @@
+/*
+ * $Id: domain.c 6988 2013-01-29 10:57:11Z matthijs $
+ *
+ * Copyright (c) 2009 NLNet Labs. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * Domain.
+ *
+ */
+
+#include "config.h"
+#include "signer/domain.h"
+#include "signer/zone.h"
+#include "util/log.h"
+
+static const char* logstr = "domain";
+
+
+/**
+ * Create domain.
+ *
+ */
+domain_type*
+domain_create(struct zone_struct* zone, dname_type* dname)
+{
+    domain_type* domain = NULL;
+    ods_log_assert(zone);
+    ods_log_assert(zone->region);
+    ods_log_assert(dname);
+    domain = (domain_type*) region_alloc(zone->region, sizeof(domain_type));
+    domain->dname = dname_clone(zone->region, dname);
+    domain->zone = zone;
+    domain->node = NULL; /* not in db yet */
+    domain->parent = NULL;
+    domain->is_apex = 0;
+    domain->is_new = 0;
+    return domain;
+}
+
+
+/**
+ * Look up RRset at this domain.
+ *
+ */
+/*
+rrset_type*
+domain_lookup_rrset(domain_type* domain, ldns_rr_type rrtype)
+{
+    rrset_type* rrset = NULL;
+    ods_log_assert(domain);
+    ods_log_assert(rrtype);
+    rrset = domain->rrsets;
+    while (rrset && rrset->rrtype != rrtype) {
+        rrset = rrset->next;
+    }
+    return rrset;
+}
+*/
+
+/**
+ * Add RRset to domain.
+ *
+ */
+/*
+void
+domain_add_rrset(domain_type* domain, rrset_type* rrset)
+{
+    rrset_type** p = NULL;
+    ods_log_assert(domain);
+    ods_log_assert(rrset);
+    if (!domain->rrsets) {
+        domain->rrsets = rrset;
+    } else {
+        p = &domain->rrsets;
+        while(*p) {
+            p = &((*p)->next);
+        }
+        *p = rrset;
+        rrset->next = NULL;
+    }
+    log_rrset(domain->dname, rrset->rrtype, "+RRSET", LOG_DEEEBUG);
+    rrset->domain = (void*) domain;
+    TODO: update denial
+    return;
+}
+*/
+
+/**
+ * Delete RRset from domain.
+ *
+ */
+/*
+rrset_type*
+domain_del_rrset(domain_type* domain, ldns_rr_type rrtype)
+{
+    rrset_type* cur = NULL;
+    ods_log_assert(domain);
+    ods_log_assert(rrtype);
+    if (!domain->rrsets) {
+        ods_log_warning("[%s] delete RRset failed: RRset with RRtype %s "
+            "does not exist", logstr, rrset_type2str(rrtype));
+        return NULL;
+    }
+    if (domain->rrsets->rrtype == rrtype) {
+        cur = domain->rrsets;
+        domain->rrsets = cur->next;
+        cur->domain = NULL;
+        cur->next = NULL;
+        log_rrset(domain->dname, rrtype, "-RRSET", LOG_DEEEBUG);
+        TODO: update denial
+        return cur;
+    }
+    cur = domain->rrsets;
+    while (cur) {
+        if (!cur->next) {
+            ods_log_warning("[%s] delete RRset failed: RRset with RRtype %s "
+                "does not exist", logstr, rrset_type2str(rrtype));
+            return NULL;
+        }
+        ods_log_assert(cur->next);
+        if (cur->next->rrtype != rrtype) {
+            cur = cur->next;
+        } else {
+            ods_log_assert(cur->next->rrtype == rrtype);
+            cur->next = cur->next->next;
+            cur = cur->next;
+            cur->domain = NULL;
+            cur->next = NULL;
+            log_rrset(domain->dname, rrtype, "-RRSET", LOG_DEEEBUG);
+            TODO: update denial
+            return cur;
+        }
+    }
+    ods_log_warning("[%s] delete RRset failed: RRset with RRtype %s "
+        "does not exist", logstr, rrset_type2str(rrtype));
+    return NULL;
+}
+*/
+
+
+/**
+ * Clean up domain.
+ *
+ */
+void
+domain_cleanup(domain_type* domain)
+{
+    return;
+}
---
+++ signer/src/signer/domain.h
@@ -0,0 +1,103 @@
+/*
+ * $Id: domain.h 6870 2012-11-27 13:01:48Z matthijs $
+ *
+ * Copyright (c) 2009 NLNet Labs. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * Domain.
+ *
+ */
+
+#ifndef SIGNER_DOMAIN_H
+#define SIGNER_DOMAIN_H
+
+#include "config.h"
+#include "dns/dname.h"
+#include "util/region.h"
+#include "util/status.h"
+#include "util/tree.h"
+
+#include <time.h>
+
+struct zone_struct;
+
+/**
+ * Domain.
+ *
+ */
+typedef struct domain_struct domain_type;
+struct domain_struct {
+    ldns_rdf* dname;
+    void* zone;
+    tree_node* node;
+    domain_type* parent;
+    unsigned is_new : 1;
+    unsigned is_apex : 1; /* apex */
+};
+
+/**
+ * Create domain.
+ * @param zone:  corresponding zone.
+ * @param dname: domain name.
+ * @return:      (domain_type*) domain.
+ *
+ */
+domain_type* domain_create(struct zone_struct* zone, dname_type* dname);
+
+/**
+ * Look up rrset at this domain.
+ * @param domain: domain.
+ * @param rrtype: RRtype.
+ * @return:       (rrset_type*) rrset, NULL if not found.
+ *
+ */
+/* rrset_type* domain_lookup_rrset(domain_type* domain, uint16_t rrtype); */
+
+/**
+ * Add rrset to domain.
+ * @param domain: domain.
+ * @param rrset:  rrset.
+ *
+ */
+/* void domain_add_rrset(domain_type* domain, rrset_type* rrset); */
+
+/**
+ * Delete RRset from domain.
+ * @param domain: domain.
+ * @param rrtype: type of rrset.
+ * @return:       (rrset_type*) deleted rrset, NULL if not found.
+ *
+ */
+/* rrset_type* domain_del_rrset(domain_type* domain, uint16_t rrtype); */
+
+/**
+ * Clean up domain.
+ * @param domain: domain to clean up.
+ *
+ */
+void domain_cleanup(domain_type* domain);
+
+#endif /* SIGNER_DOMAIN_H */
