6c3c8299c3493d0570358fb0e682251fa040681e
Matthijs Mekking <github@pletterpet.nl>
2013-07-31 19:32:08 +0200

    add rrset code

---
+++ signer/src/signer/rrset.c
@@ -0,0 +1,174 @@
+/*
+ * $Id: rrset.c 7178 2013-07-02 10:33:21Z matthijs $
+ *
+ * Copyright (c) 2009 NLNet Labs. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * RRset.
+ *
+ */
+
+#include "config.h"
+#include "util/log.h"
+#include "signer/rrset.h"
+#include "signer/zone.h"
+
+static const char* rrset_str = "rrset";
+
+
+/**
+ * Log RRset.
+ *
+ */
+void
+rrset_log(dname_type* dname, uint16_t type, const char* pre, int level)
+{
+    rrstruct_type* rrstruct = dns_rrstruct_by_type(type);
+    char str[DNAME_MAXLEN];
+    char rrtype[10];
+    dname_str(dname, &str[0]);
+    (void)snprintf(&rrtype[0], 10, "TYPE%u", (unsigned) type);
+    if (level == LOG_EMERG) {
+        ods_fatal_exit("[%s] %s: <%s,%s>",  pre?pre:"", str,
+            rrstruct->name?rrstruct->name:rrtype);
+    } else if (level == LOG_ALERT) {
+        ods_log_alert("[%s] %s: <%s,%s>",   pre?pre:"", str,
+            rrstruct->name?rrstruct->name:rrtype);
+    } else if (level == LOG_CRIT) {
+        ods_log_crit("[%s] %s: <%s,%s>",    pre?pre:"", str,
+            rrstruct->name?rrstruct->name:rrtype);
+    } else if (level == LOG_ERR) {
+        ods_log_error("[%s] %s: <%s,%s>",   pre?pre:"", str,
+            rrstruct->name?rrstruct->name:rrtype);
+    } else if (level == LOG_WARNING) {
+        ods_log_warning("[%s] %s: <%s,%s>", pre?pre:"", str,
+            rrstruct->name?rrstruct->name:rrtype);
+    } else if (level == LOG_NOTICE) {
+        ods_log_info("[%s] %s: <%s,%s>", pre?pre:"", str,
+            rrstruct->name?rrstruct->name:rrtype);
+    } else if (level == LOG_INFO) {
+        ods_log_verbose("[%s] %s: <%s,%s>", pre?pre:"", str,
+            rrstruct->name?rrstruct->name:rrtype);
+    } else if (level == LOG_DEBUG) {
+        ods_log_debug("[%s] %s: <%s,%s>", pre?pre:"", str,
+            rrstruct->name?rrstruct->name:rrtype);
+    } else if (level == LOG_DEEEBUG) {
+        ods_log_deeebug("[%s] %s: <%s,%s>", pre?pre:"", str,
+            rrstruct->name?rrstruct->name:rrtype);
+    } else {
+        ods_log_deeebug("[%s] %s: <%s,%s>", pre?pre:"", str,
+            rrstruct->name?rrstruct->name:rrtype);
+    }
+    free((void*)str);
+    return;
+}
+
+
+/**
+ * Create RRset.
+ *
+ */
+rrset_type*
+rrset_create(struct domain_struct* domain, uint16_t type)
+{
+    rrset_type* rrset = NULL;
+    ods_log_assert(domain);
+    ods_log_assert(domain->zone);
+    ods_log_assert(domain->zone->region);
+    ods_log_assert(type);
+    rrset = (rrset_type*) region_alloc(domain->zone->region,
+        sizeof(rrset_type));
+    rrset->domain = domain;
+    rrset->next = NULL;
+    rrset->rrs = NULL;
+    rrset->rrtype = type;
+    rrset->rr_count = 0;
+    rrset->needs_signing = 0;
+    return rrset;
+}
+
+
+/**
+ * Lookup RR in RRset.
+ *
+ */
+record_type*
+rrset_lookup_rr(rrset_type* rrset, rr_type* rr)
+{
+    size_t i = 0;
+    if (!rrset || !rr || rrset->rr_count <= 0) {
+       return NULL;
+    }
+    for (i=0; i < rrset->rr_count; i++) {
+        if (rr_compare_rdata(rrset->rrs[i].rr, rr) == 0) {
+            return &rrset->rrs[i];
+        }
+    }
+    return NULL;
+}
+
+
+/**
+ * Add RR to RRset.
+ *
+ */
+record_type*
+rrset_add_rr(rrset_type* rrset, rr_type* rr)
+{
+    rr_type* rrs_old = NULL;
+    zone_type* zone = NULL;
+    ods_log_assert(rrset);
+    ods_log_assert(rr);
+    ods_log_assert(rrset->rrtype == rr->type);
+    zone = (zone_type*) rrset->domain->zone;
+    rrs_old = rrset->rrs;
+    rrset->rrs = (rr_type*) region_alloc(zone->region,
+        (rrset->rr_count + 1) * sizeof(rr_type));
+    if (rrs_old) {
+        memcpy(rrset->rrs, rrs_old, (rrset->rr_count) * sizeof(rr_type));
+    }
+    rrset->rr_count++;
+    rrset->rrs[rrset->rr_count - 1].rr = rr;
+    rrset->rrs[rrset->rr_count - 1].exists = 0;
+    rrset->rrs[rrset->rr_count - 1].is_added = 1;
+    rrset->rrs[rrset->rr_count - 1].is_removed = 0;
+    rrset->needs_signing = 1;
+    return &rrset->rrs[rrset->rr_count -1];
+}
+
+
+/**
+ * Clean up RRset.
+ *
+ */
+void
+rrset_cleanup(rrset_type* rrset)
+{
+    if (!rrset) {
+       return;
+    }
+    return;
+}
---
+++ signer/src/signer/rrset.h
@@ -0,0 +1,114 @@
+/*
+ * $Id: rrset.h 6870 2012-11-27 13:01:48Z matthijs $
+ *
+ * Copyright (c) 2009 NLNet Labs. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * RRset.
+ *
+ */
+
+#ifndef SIGNER_RRSET_H
+#define SIGNER_RRSET_H
+
+#include "config.h"
+#include "dns/rr.h"
+#include "dns/dname.h"
+#include "util/status.h"
+
+struct domain_struct;
+
+/**
+ * RR structure.
+ *
+ */
+typedef struct record_struct record_type;
+struct record_struct {
+    rr_type* rr;
+    unsigned exists : 1;
+    unsigned is_added : 1;
+    unsigned is_removed : 1;
+};
+
+/**
+ * RRset structure.
+ *
+ */
+typedef struct rrset_struct rrset_type;
+struct rrset_struct {
+    rrset_type* next;
+    struct domain_struct* domain;
+    uint16_t rrtype;
+    record_type* rrs;
+    size_t rr_count;
+    unsigned needs_signing : 1;
+};
+
+/**
+ * Log rrset.
+ * @param dname: domain name.
+ * @param type:  rr type.
+ * @param pre:   log message.
+ * @param level: log level.
+ *
+ */
+void log_rrset(dname_type* dname, uint16_t type, const char* pre, int level);
+
+/**
+ * Create rrset.
+ * @param domain: corresponding domain.
+ * @param type:   rr type.
+ * @return:       (rrset_type*) rrset.
+ *
+ */
+rrset_type* rrset_create(struct domain_struct* domain, uint16_t type);
+
+/**
+ * Lookup rr in rrset.
+ * @param rrset: rrset.
+ * @param rr:    rr.
+ * @return       (record_type*) record, NULL if not found.
+ *
+ */
+record_type* rrset_lookup_rr(rrset_type* rrset, rr_type* rr);
+
+/**
+ * Add rr to rrset.
+ * @param rrset: rrset.
+ * @param rr:    rr.
+ * @return:      (record_type*) added record.
+ *
+ */
+record_type* rrset_add_rr(rrset_type* rrset, rr_type* rr);
+
+/**
+ * Clean up rrset.
+ * @param rrset: rrset to be cleaned up.
+ *
+ */
+void rrset_cleanup(rrset_type* rrset);
+
+#endif /* SIGNER_RRSET_H */
