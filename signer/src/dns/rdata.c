24facbb7a1d07d28fb9e23acb878260de7cc016c
Matthijs Mekking <github@pletterpet.nl>
2014-01-03 14:17:13 +0100

    print SIG

--- signer/src/dns/rdata.c
+++ signer/src/dns/rdata.c
@@ -31,8 +31,10 @@
  *
  */
 
+#include "compat/b64.h"
 #include "dns/dns.h"
 #include "dns/rdata.h"
+#include "dns/rr.h"
 #include "dns/wf.h"
 #include "util/util.h"
 #include "wire/buffer.h"
@@ -93,19 +95,41 @@ rdata_get_dname(rdata_type* rdata)
 
 
 /**
- * Print IPv4 RDATA element.
+ * Print base64 format RDATA element.
  *
  */
 static void
-rdata_print_ipv4(FILE* fd, rdata_type* rdata)
+rdata_print_base64(FILE* fd, rdata_type* rdata)
 {
-    char str[200];
-    /* assert fd, rdata */
-    if (inet_ntop(AF_INET, rdata_get_data(rdata), str, sizeof(str))) {
-        fprintf(fd, "%s", str);
+    region_type* tmp;
+    char* buf;
+    uint8_t* data = rdata_get_data(rdata);
+    size_t size = rdata_size(rdata);
+    int len;
+    if (size == 0) { return; }
+    tmp = region_create_custom(sizeof(region_type) + size*2 + 1);
+    if (!tmp) { return; }
+    buf = region_alloc(tmp, size*2 + 1);
+    len = b64_ntop(data, size, buf, size*2+1);
+    fprintf(fd, "%s", buf);
+    return;
+}
+
+
+/**
+ * Print datetime format RDATA element.
+ *
+ */
+static void
+rdata_print_datetime(FILE* fd, rdata_type* rdata)
+{
+    time_t data = (time_t) wf_read_uint32(rdata_get_data(rdata));
+    struct tm* tm = gmtime(&data);
+    char buf[15];
+    if (strftime(buf, sizeof(buf), "%Y%m%d%H%M%S", tm)) {
+        fprintf(fd, "%s", buf);
     } else {
-        ods_log_error("[%s] error: inet_ntop failed: %s", logstr,
-            strerror(errno));
+        fprintf(fd, "<error>");
     }
     return;
 }
@@ -151,6 +175,19 @@ rdata_print_hex(FILE* fd, rdata_type* rdata)
 
 
 /**
+ * Print int8 RDATA element.
+ *
+ */
+static void
+rdata_print_int8(FILE* fd, rdata_type* rdata)
+{
+    uint8_t data = wf_read_uint8(rdata_get_data(rdata));
+    fprintf(fd, "%u", (unsigned) data);
+    return;
+}
+
+
+/**
  * Print int16 RDATA element.
  *
  */
@@ -177,6 +214,25 @@ rdata_print_int32(FILE* fd, rdata_type* rdata)
 
 
 /**
+ * Print IPv4 RDATA element.
+ *
+ */
+static void
+rdata_print_ipv4(FILE* fd, rdata_type* rdata)
+{
+    char str[200];
+    /* assert fd, rdata */
+    if (inet_ntop(AF_INET, rdata_get_data(rdata), str, sizeof(str))) {
+        fprintf(fd, "%s", str);
+    } else {
+        ods_log_error("[%s] error: inet_ntop failed: %s", logstr,
+            strerror(errno));
+    }
+    return;
+}
+
+
+/**
  * Print nsap RDATA element.
  *
  */
@@ -185,6 +241,19 @@ rdata_print_nsap(FILE* fd, rdata_type* rdata)
 {
     fprintf(fd, "0x");
     rdata_print_hex(fd, rdata);
+    return;
+}
+
+
+/**
+ * Print rrtype RDATA element.
+ *
+ */
+static void
+rdata_print_rrtype(FILE* fd, rdata_type* rdata)
+{
+    uint16_t data = wf_read_uint16(rdata_get_data(rdata));
+    rr_print_rrtype(fd, data);
     return;
 }
 
@@ -286,24 +355,36 @@ rdata_print(FILE* fd, rdata_type* rdata, uint16_t rrtype, uint16_t pos)
         case DNS_RDATA_UNCOMPRESSED_DNAME:
             rdata_print_dname(fd, rdata);
             break;
+        case DNS_RDATA_INT8:
+            rdata_print_int8(fd, rdata);
+            break;
         case DNS_RDATA_INT16:
             rdata_print_int16(fd, rdata);
             break;
         case DNS_RDATA_INT32:
             rdata_print_int32(fd, rdata);
             break;
-        case DNS_RDATA_TEXT:
-        case DNS_RDATA_TEXTS:
-            rdata_print_text(fd, rdata);
-            break;
         case DNS_RDATA_TIMEF:
             rdata_print_timef(fd, rdata);
+            break;
+        case DNS_RDATA_DATETIME:
+            rdata_print_datetime(fd, rdata);
             break;
         case DNS_RDATA_SERVICES:
             rdata_print_services(fd, rdata);
             break;
+        case DNS_RDATA_TEXT:
+        case DNS_RDATA_TEXTS:
+            rdata_print_text(fd, rdata);
+            break;
         case DNS_RDATA_NSAP:
             rdata_print_nsap(fd, rdata);
+            break;
+        case DNS_RDATA_RRTYPE:
+            rdata_print_rrtype(fd, rdata);
+            break;
+        case DNS_RDATA_BASE64:
+            rdata_print_base64(fd, rdata);
             break;
         case DNS_RDATA_BINARY:
         default:
